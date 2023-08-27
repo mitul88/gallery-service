c1a7eb5e510704462cae285dbdc586423f9950e9
shahed <shahed.rahman88@gmail.com>
2023-08-27 22:45:59 +0600

    send role after auth

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -21,12 +21,13 @@ module.exports.register = async (req, res) => {
             user.password = await bcrypt.hash(password, salt)
             await user.save()
 
-            const token = user.generateJWT()
+            const accessToken = user.generateJWT()
 
             return res.status(201).send({
                 status: true,
                 message: "User registered",
-                token
+                roles: [user.type],
+                accessToken
             })
         } catch (error) {
             return res.status(400).send({
@@ -43,7 +44,7 @@ module.exports.login = async (req, res) => {
     const url = req.originalUrl
 
     if(!email || !password) {
-         return res.status(200).send({
+         return res.status(400).send({
              status: false,
              message: "Please fill up required information"
          })
@@ -51,23 +52,24 @@ module.exports.login = async (req, res) => {
      try{
         const user = await User.findOne({email})
         if (!user) {
-            return res.status(200).send({
+            return res.status(400).send({
                 status: false,
                 message: "Email not registered"
             })
         } else {        
             const validUser = await bcrypt.compare(req.body.password, user.password)
             if(!validUser) {
-                return res.status(200).send({
+                return res.status(400).send({
                     status: false,
                     message: "Please enter correct password"
                 })
             } else {
-                const token = user.generateJWT()
+                const accessToken = user.generateJWT()
                 return res.status(200).send({
                     status: true,
                     message: "User sign in successfull",
-                    token
+                    roles: [user.type],
+                    accessToken
                 })
             }
         }
