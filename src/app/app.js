30e197be26116ba2c147b79c5d52081570f5734a
shahed <shahed.rahman88@gmail.com>
2023-08-27 12:38:58 +0600

    cloudinaty middleware added

--- src/app/app.js
+++ src/app/app.js
@@ -3,6 +3,7 @@ require('express-async-error')
 const error = require('../middleware/error.middleware');
 const app = require('express')();
 const bodyParser = require('body-parser')
+const {cloudinaryConfig} = require('../config/cloudinary.config')
 
 require('../middleware')(app);
 require('../middleware/routes')(app);
@@ -10,6 +11,7 @@ require('../middleware/routes')(app);
 app.use(error);
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use('*', cloudinaryConfig)
 
 
 module.exports = app;
